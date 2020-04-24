befe956c41c3ea8cca9cf1a80bc2886fe48b3cdd
Caitlin <63226551+CaitlinMorey@users.noreply.github.com>
2020-04-24 18:08:15 +0800

    added forms to route and ability to create quizzes

--- routes.py
+++ routes.py
@@ -1,30 +1,44 @@
 from flask import flash, request, render_template, redirect, url_for
 from app import app
-
+from app.forms import answer
 
 @app.route('/')
-@app.route('/index')
+@app.route('/create')
+def creation():
+    form = answer()
+    
+    return render_template('create.html', form=form)
 
-def index():
+
+@app.route('/htmlquiz', methods=['GET', 'POST'])
+def htmlquiz():
+    form = answer()
     
-    limit = 2
+    return render_template('questions.html', form = form)
+
+
+@app.route('/login')
+def login():
+    name = "Caitlin"
+    return render_template('login.html', name = name)
+
+
+@app.route('/results', methods=['GET','POST'])
+def results():
+    form = answer()
+    name = "Caitlin"
+    correct = 0
+    ans = []
     
-    questions = [
-        {
-            'question': 'What holds tags',
-            'answers': ['{ }', '[ ]', '< >', '( )']
-        },
-        {
-            'question': 'what controls a websites "style"',
-            'answers': ['HTML', 'CSS', 'Javascript', 'Flask']
-        }
-    ]
+    results = request.form.to_dict()
     
-    correct = ['< >', 'CSS']
-    
-    num = len(questions)
-    
-    return render_template('questions.html', questions=questions, num=num, limit=limit)
+    ans.append(results.values())
+   
+    if ans == form.quizAnswer.data:
+        correct =+ 1
+    else:
+        correct =+ 0
+    return render_template('results.html', correct = correct, form = form, name = name)
 
 
 
